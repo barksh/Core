409a97b3f75f8163fb178aa6d029a08a15270c97
WMXPY <wm@mengw.io>
2019-03-17 20:05:57 -0500

    implement res dir controlelr

--- src/io/file.ts
+++ src/io/file.ts
@@ -7,21 +7,41 @@
 import * as Fs from 'fs';
 
 export const readTextFile = async (path: string): Promise<string> =>
-    new Promise<string>((resolve: (result: string) => void, reject: (reason: NodeJS.ErrnoException) => void) => {
+    new Promise<string>((resolve: (result: string) => void, reject: (reason: NodeJS.ErrnoException) => void) =>
         Fs.readFile(path, 'utf8', (error: NodeJS.ErrnoException, data: string) => {
+
             if (error) {
                 reject(error);
             }
             resolve(data);
-        });
-    });
+
+            return;
+        }));
 
 export const writeTextFile = async (path: string, content: string): Promise<void> =>
-    new Promise<void>((resolve: () => void, reject: (reason: NodeJS.ErrnoException) => void) => {
+    new Promise<void>((resolve: () => void, reject: (reason: NodeJS.ErrnoException) => void) =>
         Fs.writeFile(path, content, 'utf8', (error: NodeJS.ErrnoException) => {
+
             if (error) {
                 reject(error);
             }
             resolve();
-        });
-    });
+
+            return;
+        }));
+
+export const recursiveDo = async (path: string, func: (file: string) => Promise<void>): Promise<void> => {
+
+    const status: Fs.Stats = Fs.statSync(path);
+
+    if (status.isDirectory()) {
+        const files: string[] = Fs.readdirSync(path);
+        for (const file of files) {
+            await recursiveDo(file, func);
+        }
+    } else if (status.isFile()) {
+        await func(path);
+    }
+
+    return;
+};
