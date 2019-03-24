5bbd840a0ab0b275f8e3a616221b7d37efbd6d55
WMXPY <wm@mengw.io>
2019-03-24 15:46:49 -0500

    add ejs import

--- src/template/parse.ts
+++ src/template/parse.ts
@@ -4,11 +4,12 @@
  * @description Parse
  */
 
+import { render } from "ejs";
 import { TEMPLATE_METHOD } from "./declare";
 
 export const parseEjsContent = (content: string, replacement: Record<string, string>): string => {
 
-    return content;
+    return render(content, replacement);
 };
 
 export const parseContent = (method: TEMPLATE_METHOD, content: string, replacement: Record<string, string>): string => {
