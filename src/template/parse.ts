ffded600461a7ff75d154a910bfbe67d61089481
WMXPY <wm@mengw.io>
2019-03-23 22:41:07 -0500

    init parse- parse medhos

--- src/template/parse.ts
+++ src/template/parse.ts
@@ -4,3 +4,18 @@
  * @description Parse
  */
 
+import { TEMPLATE_METHOD } from "./declare";
+
+export const parseEjsContent = (content: string, replacement: Record<string, string>): string => {
+
+    return content;
+};
+
+export const parseContent = (method: TEMPLATE_METHOD, content: string, replacement: Record<string, string>): string => {
+
+    switch (method) {
+        case TEMPLATE_METHOD.EJS: return parseEjsContent(content, replacement);
+    }
+
+    return content;
+};
