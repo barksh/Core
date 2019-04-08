114c7f0204fc5b567504c6b02e1d31f4aa652c37
WMXPY <wm@mengw.io>
2019-04-07 20:00:29 -0500

    consume io, wip

--- src/io/external.ts
+++ src/io/external.ts
@@ -4,16 +4,14 @@
  * @description External
  */
 
-import { decompressZipFile, downloadFile, md5File } from "@sudoo/io";
+import { decompressZipFile, getExternalFileByProtocol, md5File } from "@sudoo/io";
 import { Environment } from "../config/environment";
-import { ERROR_CODE, panic } from "../panic/declare";
-import { EXTERNAL_PROTOCOL } from "./declare";
-import { getRandomPackagePath, getRandomTempFilePath, parseExternalProtocol, parseGithubProtocol } from "./util";
+import { getRandomPackagePath, getRandomTempFilePath } from "./util";
 
 export const downloadAndDecompress = async (env: Environment, url: string): Promise<string> => {
 
     const tempFilePath: string = await getRandomTempFilePath(env, 'zip');
-    await downloadFile(url, tempFilePath);
+    await getExternalFileByProtocol(url, tempFilePath);
 
     const hash: string = await md5File(tempFilePath);
     const packagePath: string = await getRandomPackagePath(env, hash);
@@ -22,21 +20,8 @@ export const downloadAndDecompress = async (env: Environment, url: string): Prom
     return hash;
 };
 
-export const fetchAndDecompressFromAnyExternalByProtocol = async (env: Environment, protocol: EXTERNAL_PROTOCOL, url: string): Promise<string> => {
-
-    switch (protocol) {
-        case EXTERNAL_PROTOCOL.GITHUB: {
-            const parsed: string = parseGithubProtocol(url);
-            const packageHash: string = await downloadAndDecompress(env, parsed);
-            return packageHash;
-        }
-        default: throw panic.code(ERROR_CODE.NOT_IMPLEMENTED);
-    }
-};
-
 export const fetchAndDecompressFromAnyExternal = async (env: Environment, url: string): Promise<string> => {
 
-    const protocol: EXTERNAL_PROTOCOL = parseExternalProtocol(url);
-    const packageHash: string = await fetchAndDecompressFromAnyExternalByProtocol(env, protocol, url);
+    const packageHash: string = await downloadAndDecompress(env, url);
     return packageHash;
 };
